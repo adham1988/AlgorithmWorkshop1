4c148742c717fe5093987080dc896c419cb4e10f
adham1988 <ataha18@student.aau.dk>
2019-10-25 08:32:25 +0200

    with comments

--- workshop.py
+++ workshop.py
@@ -3,22 +3,22 @@ class Car:                                #initializing class Car with three obj
     id = None
     x = None
     y = None
-ListOfData=[]
+ListOfData=[]                            #decaring a list to store data in
 data_file = open("smalldata.txt", "r")   #declaring new variable with 2 arguments, the file we want to open and the type
-                                         #of permission needed. in this case r, which is read from file.
+                                         #of permission needed. in this case r, which is read
+# from file.
 g = True
 for data in data_file.readlines():
     if g is True:                        #this variable was used to skip the first line
         g = False
     else:
-        arr = data.replace("\n", "").split(";")            #the data is splitet by ;
-        Car.id = int(arr[0])                #
-        Car.x = float(arr[1])
-        Car.y = float(arr[2])
-        arr = [int(arr[0]), float(arr[1]), float(arr[2])]
-        ListOfData.append(arr)
-
-data_file.close()
+        arr = data.replace("\n", "").split(";")  # removing th unnecessary symboles and the data is splitet by ;
+        Car.id = int(arr[0])            ##change the format of data to intiger
+        Car.x = float(arr[1])           #change the format of data to float
+        Car.y = float(arr[2])           ##change the format of data to float
+        arr = [int(arr[0]), float(arr[1]), float(arr[2])] #set the extracted data in the following variables
+        ListOfData.append(arr)          #add every data line to the List
+data_file.close()                       #close the extern file
 print(ListOfData[1])
 
 
