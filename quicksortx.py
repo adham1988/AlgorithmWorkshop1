19dc8b67c5f4c40f34b84cbbf12af9e20df50728
Fairgrove <54886528+Fairgrove@users.noreply.github.com>
2019-10-28 12:24:29 +0100

    Add files via upload

--- quicksortx.py
+++ quicksortx.py
@@ -1,12 +1,14 @@
 #Read Data
 import time
+from datetime import datetime
 class Car:                                #initializing class Car with three objects id, x and y
     id = None
     x = None
     y = None
 ListOfData=[]
-data_file = open("smalldata.csv", "r")   #declaring new variable with 2 arguments, the file we want to open and the type
-                                         #of permission needed. in this case r, which is read from file.
+data_file = open("bigdata.csv", "r")   #declaring new variable with 2 arguments, the file we want to open and the type
+
+t3 = datetime.now()                                         #of permission needed. in this case r, which is read from file.
 g = True
 for data in data_file.readlines():
     if g is True:                        #this variable was used to skip the first line
@@ -20,27 +22,8 @@ for data in data_file.readlines():
         ListOfData.append(arr)
 
 data_file.close()
-
-
-
-def largex ():
-    xmax = ListOfData[0][1]
-    for i in range(len(ListOfData)-1):
-        if ListOfData[i][1] >= xmax:
-           xmax = ListOfData[i][1]       
-    print("xmax: ", xmax)
-    return xmax
-
-def minix ():
-    xmin = ListOfData[0][1]
-    for i in range(len(ListOfData)-1):
-        if ListOfData[i][1] <= xmin:
-           xmin = ListOfData[i][1]
-    print("xmin: ", xmin)
-    return xmin
-
-
-
+t4 = datetime.now()
+print(t4 - t3)
 
 
 def partitionx(ListOfData, low, high):
@@ -50,10 +33,9 @@ def partitionx(ListOfData, low, high):
     for j in range(low, high):
         if ListOfData[j][1] <= pivot:
             i = i+1
-            #print(j)
-            ListOfData[i][1],ListOfData[j][1] = ListOfData[j][1],ListOfData[i][1]
-    #print(i)
-    ListOfData[i+1][1],ListOfData[high][1] = ListOfData[high][1],ListOfData[i+1][1]
+            ListOfData[i],ListOfData[j] = ListOfData[j],ListOfData[i]
+    ListOfData[i+1],ListOfData[high] = ListOfData[high],ListOfData[i+1]
+    #print(i+1)
     return (i+1)
 
 def quicksortx(ListOfData, low, high):
@@ -61,13 +43,18 @@ def quicksortx(ListOfData, low, high):
         pi = partitionx(ListOfData,low,high)
         quicksortx(ListOfData,low,pi-1)
         quicksortx(ListOfData,pi+1,high)
-        
-x=0
+
+
 low = 0
 high = len(ListOfData)-1
-#high = 39999
+t1 = datetime.now()
 quicksortx(ListOfData, low, high)
-for i in range(len(ListOfData)-1):
-    print (i,"  ",ListOfData[i][1])
+t2 = datetime.now()
+
+print(t2 - t1)
+#ListOfData[high][1]
+print("smallest:", ListOfData[0], "biggest", ListOfData[len(ListOfData)-1])
+#for i in range(len(ListOfData)-1):
+#    print (ListOfData[i][0],"  ",ListOfData[i][1],"  ",ListOfData[i][2])
 #print(len(ListOfData)-1)
 
