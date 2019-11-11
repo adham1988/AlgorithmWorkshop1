ea7ea38ea1144a8c07c8986383b70aeb2d3df180
Abraham Antonisen <31692509+aanton14@users.noreply.github.com>
2019-11-11 08:43:04 +0100

    Uploaded new code

---
+++ AlgWorkshop1/LocationData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Workshop_1_Algoritmer
+{
+    /// <summary>
+    /// The class for encapsulating the location data of a specific car
+    /// </summary>
+    public class LocationData
+    {
+        public int id;
+        public double x, y;
+
+        //The class constructor
+        public LocationData(int id, double x, double y)
+        {
+            this.id = id;
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
